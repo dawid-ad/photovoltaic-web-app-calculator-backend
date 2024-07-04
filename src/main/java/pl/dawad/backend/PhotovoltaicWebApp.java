35067d4fb47bd2b042ed2efedbe3ba30c942b9bf
Dawid Adachowski <dawid.adachowski@spsglobal.com>
2024-07-04 20:01:12 +0200

    init commit

---
+++ src/main/java/pl/dawad/backend/PhotovoltaicWebApp.java
@@ -0,0 +1,13 @@
+package pl.dawad.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotovoltaicWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotovoltaicWebApp.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dawad/backend/PhotovoltaicWebAppTests.java
@@ -0,0 +1,13 @@
+package pl.dawad.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotovoltaicWebAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
